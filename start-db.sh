c8ccc699f811c9cc13dc9a53d8f545f48ad4c499
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-04 16:46:01 +0300

    up

--- start-db.sh
+++ start-db.sh
@@ -11,11 +11,23 @@ KEY_VALUE_DB="key-value-db"
 KEY_VALUE_USER="key-value-user"
 KEY_VALUE_PASSWORD="key-value-password"
 
+# Conectivity
+LOCALHOST_PORT=27017
+CONTAINER_PORT=27017
+NETWORK_NAME="key-value-net"
+
+# Storage(volume)
+VOLUME_NAME="key-value-data"
+VOLUME_CONTAINER_PATH="/data/db"
+
 docker run --rm -d --name $CONTAINER_NAME \
     -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
     -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
     -e KEY_VALUE_DB=$KEY_VALUE_DB \
     -e KEY_VALUE_USER=$KEY_VALUE_USER \
     -e KEY_VALUE_PASSWORD=$KEY_VALUE_PASSWORD \
+    -p $LOCALHOST_PORT:$CONTAINER_PORT \
+    -v $VOLUME_NAME:$VOLUME_CONTAINER_PATH \
     -v ./db-config/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js:ro \
+    --network $NETWORK_NAME \
     $MONGODB_IMAGE:$MONGODB_TAG
