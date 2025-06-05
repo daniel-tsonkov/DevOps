cf0f2a4d3ec0069cd67820dd489c9fdf02d1ecc2
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-05 11:06:43 +0300

    up

--- start-db.sh
+++ start-db.sh
@@ -20,6 +20,14 @@ CONTAINER_PORT=27017
 source .env.volume
 VOLUME_CONTAINER_PATH="/data/db"
 
+source setup.sh
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "A container with the name $CONTAINER_NAME already exists."
+    echo "The container will be removed when stopped."
+    echo "To stop the container, run: docker kill $CONTAINER_NAME"
+fi
+
 docker run --rm -d --name $CONTAINER_NAME \
     -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
     -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
