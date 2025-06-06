e56f84ef19d71dd625cb42d7d2f96fa98c6581f7
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-06 10:37:06 +0300

    up

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,30 @@
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-
-mongoose.connect("mongodb://mongodb/key-value-db", {
-  auth: {
-    username: "key-value-user",
-    password: "key-value-password",
-  },
-  connectTimeoutMS: 500,
+app.get("/health", (req, res) => {
+  res.status(200).send("up");
 });
+
+console.log("Connecting to DB");
+
+mongoose
+  .connect("mongodb://mongodb/key-value-db", {
+    auth: {
+      username: "key-value-user",
+      password: "key-value-password",
+    },
+    connectTimeoutMS: 500,
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Listening on port 3000");
+    });
+    console.log("Connected to db");
+  })
+  .catch((err) => {
+    console.error("Something went wrong");
+    console.log(err);
+  });
