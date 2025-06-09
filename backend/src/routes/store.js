68ea18d84400ef2e64b52da2dbed53f2d6d6c805
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-09 15:20:15 +0300

    up

--- backend/src/routes/store.js
+++ backend/src/routes/store.js
@@ -7,7 +7,9 @@ keyValueRouter.post("/", async (req, res) => {
   const { key, value } = req.body;
 
   if (!key || !value) {
-    return res.status(400).json({ error: "Both key and value are required" });
+    return res
+      .status(400)
+      .json({ error: "Both 'key' and 'value' are required" });
   }
 
   try {
@@ -26,24 +28,51 @@ keyValueRouter.post("/", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-keyValueRouter.get("/:key", (req, res) => {
+
+keyValueRouter.get("/:key", async (req, res) => {
   const { key } = req.params;
 
   try {
-    console.log(key);
-    return res.status(200).json({ message: "getting key-value pair" });
+    const keyValue = await KeyValue.findOne({ key });
+
+    if (!keyValue) {
+      return res.status(404).json({ error: "Key not found" });
+    }
+
+    return res.status(200).json({ key, value: keyValue.value });
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-keyValueRouter.put("/:key", (req, res) => {
+
+keyValueRouter.put("/:key", async (req, res) => {
+  const { key } = req.params;
+  const { value } = req.body;
+
+  if (!value) {
+    return res.status(400).json({ error: "'value' is required" });
+  }
   try {
+    const keyValue = await KeyValue.findOneAndUpdate(
+      { key },
+      { value },
+      { new: true }
+    );
+
+    if (!keyValue) {
+      return res.status(404).json({ error: "Key not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Key-value pair updated successfuly", key, value });
   } catch (err) {
     res.status(500).json({ message: "Internal server error" });
   }
 
   return res.send("updating key-value pair");
 });
+
 keyValueRouter.delete("/:key", (req, res) => {
   try {
   } catch (err) {
