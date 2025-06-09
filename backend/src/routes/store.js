054a72de9b4ceb068c068bae0a3f3946d24876ac
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-09 14:02:52 +0300

    up

--- backend/src/routes/store.js
+++ backend/src/routes/store.js
@@ -1,20 +1,43 @@
-const express = require('express');
+const express = require("express");
+const { KeyValue } = require("../models/keyValue");
 
 const keyValueRouter = express.Router();
 
-keyValueRouter.post('/', (req, res) => {
-    return res.send('creating key-value pair')
+keyValueRouter.post("/", (req, res) => {
+  const { key, value } = req.body;
+
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+  return res.send("creating key-value pair");
 });
-keyValueRouter.get('/:key', (req, res) => {
-    return res.send('getting key-value pair')
+keyValueRouter.get("/:key", (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+  return res.send("getting key-value pair");
 });
-keyValueRouter.put('/:key', (req, res) => {
-    return res.send('updating key-value pair')
+keyValueRouter.put("/:key", (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+  return res.send("updating key-value pair");
 });
-keyValueRouter.delete('/:key', (req, res) => {
-    return res.send('deleting key-value pair')
+keyValueRouter.delete("/:key", (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+  return res.send("deleting key-value pair");
 });
 
 module.exports = {
-    keyValueRouter,
+  keyValueRouter,
 };
