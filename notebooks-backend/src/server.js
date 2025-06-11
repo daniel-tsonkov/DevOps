09612bccdcff44b5759dc88b98572f04f4238dea
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-11 22:42:51 +0300

    up

--- notebooks-backend/src/server.js
+++ notebooks-backend/src/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "Hello from notebooks" }));
+app.get("/", (req, res) => res.json({ message: "Hello from notebooksNEW" }));
 
 const port = process.env.PORT;
 
