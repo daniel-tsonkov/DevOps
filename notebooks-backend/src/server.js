6f7b4905014a6932bf58bf33bcff236b01a9af2a
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-11 08:50:59 +0300

    up

--- notebooks-backend/src/server.js
+++ notebooks-backend/src/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.get("/", (req, res) => res.json({ message: "Hello from notebooks" }));
 
 const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Notebooks server listening on port ${port}`);
-});
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB! Starting server.");
+
+    app.listen(port, () => {
+      console.log(`Notebooks server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("ERROR!!!");
+    console.error(err);
+  });
