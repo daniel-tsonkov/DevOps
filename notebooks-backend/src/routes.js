0864d7b47470050b47eda63aafb2338bfff38ff1
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-06-18 21:41:23 +0300

    up

--- notebooks-backend/src/routes.js
+++ notebooks-backend/src/routes.js
@@ -38,19 +38,67 @@ notebookRouter.get("/", async (req, res) => {
 notebookRouter.get("/:id", async (req, res) => {
   try {
     const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "Notebook not found" });
+    }
+
     const notebook = await Notebook.findById(id);
 
     if (!notebook) {
       return res.status(404).json({ error: "Notebook not found" });
     }
+
     return res.status(200).json({ data: notebook });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
-notebookRouter.put("/:id", async (req, res) => {});
-notebookRouter.delete("/:id", async (req, res) => {});
+notebookRouter.put("/:id", async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "Notebook not found" });
+    }
+
+    const notebook = await Notebook.findByIdAndUpdate(
+      id,
+      { name, description },
+      { new: true }
+    );
+
+    if (!notebook) {
+      return res.status(404).json({ error: "Notebook not found" });
+    }
+
+    return res.json({ data: notebook });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+notebookRouter.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ error: "Notebook not found" });
+    }
+
+    const notebook = await Notebook.findByIdAndDelete(id);
+
+    if (!notebook) {
+      return res.status(404).json({ error: "Notebook not found" });
+    }
+
+    return res.sendStatus(204);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = {
   notebookRouter,
